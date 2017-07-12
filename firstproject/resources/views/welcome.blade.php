fc966cec59f7e32679de34731e7448915f33a89a
Siet <siet.sophort60@gmail.com>
2017-07-12 15:33:38 +0700

    Second commit

--- firstproject/resources/views/welcome.blade.php
+++ firstproject/resources/views/welcome.blade.php
@@ -1,95 +1,165 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+<head>
+    <title>Slash-Task-002</title>
+    <link href="{{asset('../css\styleWelcome.css')}}" type="text/css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+  <div class="Container">
+      <div class="Header">
+              <img src="../image/ima.png" width="100%" height="500px">
+          <div class="Logo">
+              <div class ="SpanLogo" >
+                  <span style="background-color: yellow">Y</span>
+                  <span style="background-color: darkcyan">S</span>
+                  <span style="background-color: darkgreen">L</span>
+                  <p style="font-size: 10px">YSL@Company</p>
+                  </div>
+              </div>
 
-        <title>Laravel</title>
+         <div class="Menu">
+             <ul>
+                 <li><a style="margin-left:70px">About Us</a></li>
+                 <li><a>Product& Services</a></li>
+                 <li><a>Quote Request</a></li>
+                 <li><a style="margin-left: -15px">Contact Us</a></li>
+             </ul>
+         </div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+          <div class="Text">
+              <p>YELLOW SEA LOGISTICS offers the supports for the foods transportation both<br>
+                  locally and globally such as Rail Freight, Intermodal, Air Freight, Ocean Freight,
+                  <br>Custom Broker and Warehousing /Distribution
+              </p>
+              <button class="ButtonHead">QUOTE REQUEST</button>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+          </div>
+         </div>
+      <div class="Body">
 
-            .full-height {
-                height: 100vh;
-            }
+            <p>About<span>yellowsea Logistic</span></p>
+            <p>
+                Yellow Sea Logistic offers the supports for the goods transportation both locally and globally such as Rail Freight,
+                Intermodal,Air Freight,Ocean Freight,Custom Broker,and Warehousing/ Distribution.<br>
+            </p>
+            <p>
+                Based on our strong assets and facilities,we have been motivated to start our first service providing in 2016,
+                and then with many more services from the start to the end.We would like our customer to rest assured and let us take care of the rest for you.
+                 We are new in term of company, but we have a talented and experienced team here to ensure a high quality service being delivered and satisfied
+                to our customers. <br/>
+            </p>
+            <p>
+                We are doing our best as a responsible business partner and a customer focused service to bring value and solution to your business
+                          develoment as well as ours.
+            </p>
+            <div class="Image">
+              <img src="../image/ima.png" style="width:100%; height:300px;">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+              <div class="LogoImage">
+                <span style="background-color: yellow">Y</span>
+                <span style="background-color: darkcyan">S</span>
+                <span style="background-color: darkgreen">L</span>
+                <p style="font-size: 10px">YSL@Company</p>
+              </div>
 
-            .position-ref {
-                position: relative;
-            }
+            </div>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+            <h1><p>PRODUCT & SERVICE</p><hr width="15%" style="background-color: darkorange"></h1>
 
-            .content {
-                text-align: center;
-            }
+            <div class="IntermodalService">
+                <p><h2>Intermodal Service</h2></p>
+                <p>Our core product is to provide you the stable but qualified delivery service.
+                   We have a station where you can easily drop your cargo and leave everything to us. On top of that,We have the intermodal
+                   service to support you an all-in delivery to your warehouse directly.<br/>
+                <p>We have a strong product designed as we have a warehous combined with local
+                       intermodal service. It will save you time, cost and security.Your cargos will delivered at the destination all at the same time.
+                </p><hr style="background-color: darkorange">
+            </div>
 
-            .title {
-                font-size: 84px;
-            }
+            <div class="IntermodalImage">
+                <img src="../image/404.png" width="95%" height="160px" style="margin-left: 20px; margin-top:15px;">
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+            </div>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
+            <div class="FreightForwarding">
+                <p><h2> Freight Forwarding</h2></p>
+                <p>By Ocean or Air, FCL or LCL,we believe that we are the one for you.We focus to offer the fast
+                   and premium service from Door to Door/Port to Port/Door to Port/Port to Door.<br>
+                </p>
+                <p>We desing our service as per your individual requirements from/to almost anywhere
+                   you wish to ship your cargo in the globe.We handle them with Care and Responsibility.
+                   Given your available time range, we will support you to present your cargos to anypalce
+                   of your request with the cost effectiveness.
+                </p><hr style="background-color: darkorange">
+            </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+            <div class="FrieghtImage">
+                 <img src="../image/404.png" width="95%" height="160px" style="margin-left: -20px;">
+            </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            <div class="CustomHouseBrokerage">
+                <p><h2>Custom House Brokerage</h2></p>
+                <p>For ease of doing your trading business,we urge to provide you -Custom
+                   House Brokerage service supporting your imports businesses.<br>
+                </p>
+                <p>We will cover all the praparations of required documents, taxes/duties caculation and
+                   payment, ensuring customs fomality completion in the fast maner, so your cargo will
+                   be available as planned.
+                </p><hr/>
+
+            </div>
+
+            <div class="CustomImage">
+
+                <img src="../image/404.png" width="95%" height="160px" style="margin-left: 20px; margin-top: 15px;">
+            </div>
+
+            <div class="WarehousingDistibution">
+                <p><h2>Warehousing and Distibution</h2></p>
+                <p>Storage is crucially important for your cargos as it is the starting before
+                   bringing your cargos to the world or to your retails shop.
+                </p>
+                <p>We keep it a priority when it comes to this. That is why we have our own
+                   warehuose which is standardize, highly maintianed and ideally safe. There is also our value
+                   added service to distribute your wish-list cargo to you whether it is in a small or big volume.
+                </p><hr/>
+            </div>
+
+            <div class="WarehousingImage">
+
+                    <img src="../image/404.png" width="95%" height="160px" style="margin-left:-20px; margin-top: 10px;">
+            </div>
+
+            <div class="ImagenearFooter">
+                <img src="../image/ima1.png" width="100%" height="250px">
+                <div class="TextnearFooter">
+                    <p>YELLOW SEA LOGISTICS offers the supports for the foods transportation both<br>
+                       locally and globally such as Rail Freight, Intermodal, Air Freight, Ocean Freight,
+                       <br>Custom Broker and Warehousing /Distribution</p>
+                    <button class="ButtonnearFooter">QUOTE REQUEST</button>
+
                 </div>
             </div>
-        </div>
-    </body>
+      </div>
+
+      <div class="Footer">
+          <h1>FOLLOW US</h1>
+           <!-- Add font awesome icons-->
+          <a href="#" class="fa fa-facebook" style="margin-left:400px;"></a>
+          <a href="#" class="fa fa-twitter"></a>
+          <a href="#" class="fa fa-youtube"></a>
+          <a href="#" class="fa fa-google"></a>
+          <a href="#" class="fa fa-instagram"></a>
+
+          <div class="FooterItem">
+              <a style="margin-left:300px">About Us </a>
+              <a style="margin-left:-50px">Quote Request</a>
+              <a>Products & Service</a>
+              <a>Contact Us</a>
+              <p>@2017 Yellow Sea Logistics.All right reserved Designed by Flexitech</p>
+          </div>
+      </div>
+
+</div>
+</body>
 </html>
