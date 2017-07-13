a245404570170f04b5631b2e3c213f30f8985629
Siet <siet.sophort60@gmail.com>
2017-07-13 15:19:21 +0700

    update commit3

--- firstproject/resources/views/welcome.blade.php
+++ firstproject/resources/views/welcome.blade.php
@@ -1,30 +1,70 @@
 <html>
 <head>
     <title>Slash-Task-002</title>
+    <script type="text/javascript" src="{{asset('bootstrap-3.3.7-dist/js/bootstrap.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('bootstrap-3.3.7-dist/js/jquery-3.2.1.js')}}"></script>
+    <link href="{{asset('bootstrap-3.3.7-dist/css/bootstrap.min.css')}}" type="text/css" rel="stylesheet">
     <link href="{{asset('../css\styleWelcome.css')}}" type="text/css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
 <body>
-  <div class="Container">
+
       <div class="Header">
-              <img src="../image/ima.png" width="100%" height="500px">
+              <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                  <!-- Indicators -->
+                  <ol class="carousel-indicators">
+                      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                      <li data-target="#myCarousel" data-slide-to="1"></li>
+                      <li data-target="#myCarousel" data-slide-to="2"></li>
+                  </ol>
+
+                  <!-- Wrapper for slides -->
+                  <div class="carousel-inner">
+                      <div class="item active">
+                          <img src="image/ima1.png"style="width:100%;height: 500px;">
+                      </div>
+
+                      <div class="item">
+                          <img src="image/ima.png" style="width:100%; height: 500px">
+                      </div>
+
+                      <div class="item">
+                          <img src="image/404.png"  style="width:100%;height: 500px;">
+                      </div>
+                  </div>
+
+                  <!-- Left and right controls -->
+                  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                      <span class="glyphicon glyphicon-chevron-left"></span>
+                      <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                      <span class="glyphicon glyphicon-chevron-right"></span>
+                      <span class="sr-only">Next</span>
+                  </a>
+              </div>
           <div class="Logo">
               <div class ="SpanLogo" >
                   <span style="background-color: yellow">Y</span>
                   <span style="background-color: darkcyan">S</span>
                   <span style="background-color: darkgreen">L</span>
-                  <p style="font-size: 10px">YSL@Company</p>
+                  <p style="font-size:15px; margin-top:10px;">YSL@Company</p>
                   </div>
               </div>
 
-         <div class="Menu">
-             <ul>
-                 <li><a style="margin-left:70px">About Us</a></li>
-                 <li><a>Product& Services</a></li>
-                 <li><a>Quote Request</a></li>
-                 <li><a style="margin-left: -15px">Contact Us</a></li>
-             </ul>
-         </div>
+          <div class="NavigationList">
+              <ul>
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Product &amp; Services</a></li>
+                  <li><a href="#"> Quote Request</a></li>
+                  <li><a href="#">Contact Us</a></li>
+              </ul>
+          </div>
 
           <div class="Text">
               <p>YELLOW SEA LOGISTICS offers the supports for the foods transportation both<br>
@@ -35,23 +75,27 @@
 
           </div>
          </div>
+      </div>
       <div class="Body">
 
-            <p>About<span>yellowsea Logistic</span></p>
-            <p>
-                Yellow Sea Logistic offers the supports for the goods transportation both locally and globally such as Rail Freight,
-                Intermodal,Air Freight,Ocean Freight,Custom Broker,and Warehousing/ Distribution.<br>
-            </p>
-            <p>
-                Based on our strong assets and facilities,we have been motivated to start our first service providing in 2016,
-                and then with many more services from the start to the end.We would like our customer to rest assured and let us take care of the rest for you.
-                 We are new in term of company, but we have a talented and experienced team here to ensure a high quality service being delivered and satisfied
-                to our customers. <br/>
-            </p>
-            <p>
-                We are doing our best as a responsible business partner and a customer focused service to bring value and solution to your business
-                          develoment as well as ours.
-            </p>
+          <div class="jumbotron">
+              <div class="container">
+                <p>About<span>yellowsea Logistic</span></p>
+                <p>
+                    Yellow Sea Logistic offers the supports for the goods transportation both locally and globally such as Rail Freight,
+                    Intermodal,Air Freight,Ocean Freight,Custom Broker,and Warehousing/ Distribution.<br>
+                </p>
+                <p>
+                    Based on our strong assets and facilities,we have been motivated to start our first service providing in 2016,
+                    and then with many more services from the start to the end.We would like our customer to rest assured and let us take care of the rest for you.
+                     We are new in term of company, but we have a talented and experienced team here to ensure a high quality service being delivered and satisfied
+                    to our customers. <br/>
+                </p>
+                <p>
+                    We are doing our best as a responsible business partner and a customer focused service to bring value and solution to your business
+                              develoment as well as ours.
+                </p>
+              </div>
             <div class="Image">
               <img src="../image/ima.png" style="width:100%; height:300px;">
 
@@ -59,12 +103,12 @@
                 <span style="background-color: yellow">Y</span>
                 <span style="background-color: darkcyan">S</span>
                 <span style="background-color: darkgreen">L</span>
-                <p style="font-size: 10px">YSL@Company</p>
+                <p style="font-size:10px">YSL@Company</p>
               </div>
-
-            </div>
-
-            <h1><p>PRODUCT & SERVICE</p><hr width="15%" style="background-color: darkorange"></h1>
+                </div>
+          </div>
+          <div class="feature">
+            <h1><p>PRODUCT & SERVICE</p><hr width="15%" style=" border: 1px solid darkorange;"></h1>
 
             <div class="IntermodalService">
                 <p><h2>Intermodal Service</h2></p>
@@ -73,7 +117,7 @@
                    service to support you an all-in delivery to your warehouse directly.<br/>
                 <p>We have a strong product designed as we have a warehous combined with local
                        intermodal service. It will save you time, cost and security.Your cargos will delivered at the destination all at the same time.
-                </p><hr style="background-color: darkorange">
+                </p><hr>
             </div>
 
             <div class="IntermodalImage">
@@ -129,37 +173,35 @@
 
                     <img src="../image/404.png" width="95%" height="160px" style="margin-left:-20px; margin-top: 10px;">
             </div>
-
+          </div>
+      </div>
             <div class="ImagenearFooter">
-                <img src="../image/ima1.png" width="100%" height="250px">
+                <img src="../image/ima1.png" width="100%" height="350px">
                 <div class="TextnearFooter">
                     <p>YELLOW SEA LOGISTICS offers the supports for the foods transportation both<br>
                        locally and globally such as Rail Freight, Intermodal, Air Freight, Ocean Freight,
                        <br>Custom Broker and Warehousing /Distribution</p>
                     <button class="ButtonnearFooter">QUOTE REQUEST</button>
-
                 </div>
-            </div>
-      </div>
 
-      <div class="Footer">
+       <div class="Footer">
+
           <h1>FOLLOW US</h1>
            <!-- Add font awesome icons-->
-          <a href="#" class="fa fa-facebook" style="margin-left:400px;"></a>
+          <a href="#" class="fa fa-facebook" style="margin-left:500px;"></a>
           <a href="#" class="fa fa-twitter"></a>
-          <a href="#" class="fa fa-youtube"></a>
-          <a href="#" class="fa fa-google"></a>
+          <a href="#" class="fa fa-google-plus"></a>
           <a href="#" class="fa fa-instagram"></a>
+          <a href="#" class="fa fa-youtube"></a>
 
           <div class="FooterItem">
-              <a style="margin-left:300px">About Us </a>
+              <a style="margin-left:400px">About Us </a>
               <a style="margin-left:-50px">Quote Request</a>
               <a>Products & Service</a>
               <a>Contact Us</a>
               <p>@2017 Yellow Sea Logistics.All right reserved Designed by Flexitech</p>
           </div>
       </div>
-
-</div>
+            </div>
 </body>
 </html>
