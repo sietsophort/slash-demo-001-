9be823d3a3497643c137f7fb7f0bc3bcd1167bf6
Siet <siet.sophort60@gmail.com>
2017-07-19 16:24:34 +0700

    update on TheSlash-002_YSL

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0 , maximum-scale=1.0, user-scalable=no">
-
-
-    <title itemprop='name'>YSL TheSlash</title>
-    <link href="http://yellowlg.demo.flexitech.io/img/favicon.ico" type="image/x-icon" rel="shortcut icon" />
+    <title>YSL TheSlash</title>
     <link href="{{asset('bootstrap-3.3.7-dist/css/bootstrap.min.css')}}" type="text/css" rel="stylesheet">
     <link href="{{asset('../css\styleWelcome.css')}}" type="text/css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
